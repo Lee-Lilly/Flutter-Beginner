1c939635eeec93c9e55356a7ac3f6b6b5917c9ba
P3025 <p3025@student.jamk.fi>
2020-08-15 15:41:55 +0300

    grouped class Ninja and function Fetch() in separate dart file'ninja.dart', which can be imported into 'main.dart'

--- id_card/README.md
+++ id_card/README.md
@@ -6,11 +6,20 @@ A new Flutter application.
 
 This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
+Title: Ninja ID Card
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+Main Function: 
+	1. HttpRequest to usersApi; 
+	2. Fetch data asynchronously; 
+	3. Convert repsonse in json form into Dart object;
+	4. Looping through all users by incremental of userID,
+	5. Display data in Flutter UI Widgets.
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Main Flutter UI widgets: 
+	1. AppBar;
+	2. FutureBuilder for displaying response;
+	3. FloatActionButton for looping through all users.
+	
+Main Dart components:
+	1. main.dart (class IDCard, _IDCardState)
+	2. ninja.dart (class Ninja, function fetchNinja(id))
--- id_card/lib/main.dart
+++ id_card/lib/main.dart
@@ -1,52 +1,6 @@
 import 'dart:ui';
-import 'dart:async';
-//dart:convert package convert http response to Dart object
-import 'dart:convert';
-// http package is added to dependencies in pubspec.yaml
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-
-
-
-class Ninja {
-  //create a Ninja class that contains the data from the network request.
-  final int id;
-  final String firstName;
-  final String lastName;
-  final String email;
-  final String imgUrl;
-
-  // constructor with named parameters
-  // & automatically assigns named arguments to class properties
-  Ninja({this.id, this.firstName, this.lastName, this.email, this.imgUrl});
-
-  // factory constructor creates a Ninja from JSON.
-  factory Ninja.fromJson(Map<String, dynamic> json) {
-    return Ninja(
-      id: json['data']['id'],
-      firstName: json['data']['first_name'],
-      lastName: json['data']['last_name'],
-      email: json['data']['email'],
-      imgUrl: json['data']['avatar'],
-    );
-  }
-}
-
-// Future is a core Dart class for working with async operations.
-Future<Ninja> fetchNinja(id) async {
-  // make a http request to a userApi, with parameter 'id'.
-  final response = await http.get('https://reqres.in/api/users/'+'$id');
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Ninja.fromJson(json.decode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
+import 'ninja.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -62,7 +16,6 @@ void main() {
 }
 
 class IdCard extends StatefulWidget {
-  
   @override
   _IdCardState createState() => _IdCardState();
 }
