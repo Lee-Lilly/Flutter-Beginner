54212ab7fe79428e6d73ee320177957fe81d626e
P3025 <p3025@student.jamk.fi>
2020-08-15 11:56:33 +0300

    Future module is moved out of StatefulWidget, it is called in Future Builder by passing a parameter 'id'. Pressing floatActionButton will increase 'id', and setState of new 'id'

--- id_card/lib/main.dart
+++ id_card/lib/main.dart
@@ -2,20 +2,22 @@ import 'dart:ui';
 import 'dart:async';
 //dart:convert package convert http response to Dart object
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 // http package is added to dependencies in pubspec.yaml
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
 
 
 class Ninja {
+  //create a Ninja class that contains the data from the network request.
   final int id;
   final String firstName;
   final String lastName;
   final String email;
   final String imgUrl;
 
-  //create a Ninja class that contains the data from the network request.
+  // constructor with named parameters
+  // & automatically assigns named arguments to class properties
   Ninja({this.id, this.firstName, this.lastName, this.email, this.imgUrl});
 
   // factory constructor creates a Ninja from JSON.
@@ -27,6 +29,22 @@ class Ninja {
       email: json['data']['email'],
       imgUrl: json['data']['avatar'],
     );
+  }
+}
+
+// Future is a core Dart class for working with async operations.
+Future<Ninja> fetchNinja(id) async {
+  // make a http request to a userApi, with parameter 'id'.
+  final response = await http.get('https://reqres.in/api/users/'+'$id');
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Ninja.fromJson(json.decode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
   }
 }
 
@@ -44,7 +62,7 @@ void main() {
 }
 
 class IdCard extends StatefulWidget {
-  // This widget is the root of your application.
+  
   @override
   _IdCardState createState() => _IdCardState();
 }
@@ -52,33 +70,10 @@ class IdCard extends StatefulWidget {
 class _IdCardState extends State<IdCard> {
   int id = 1;
 
-  // Future is a core Dart class for working with async operations.
-  Future<Ninja> fetchNinja() async {
-    final response = await http.get('https://reqres.in/api/users/'+'$id');
-
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      return Ninja.fromJson(json.decode(response.body));
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load album');
-    }
-  }
-
-//  Future<Ninja> futureNinja;
-//
-//  @override
-//  void initState() { //initial state by calling fetch method
-//    super.initState();
-//    futureNinja = fetchNinja();
-//  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.indigo[300],
+        backgroundColor: Colors.indigo[200],
         appBar: AppBar(
           title: Text('Ninja ID Card'),
           centerTitle: true,
@@ -133,10 +128,10 @@ class _IdCardState extends State<IdCard> {
 
       body: Padding(
         padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
-        child: FutureBuilder<Ninja>(
-                  future: fetchNinja(),
-                  builder: (context, snapshot) {
-                    if(snapshot.hasData){
+        child: FutureBuilder<Ninja>( //use the FutureBuilder widget to render data
+                  future: fetchNinja(id),
+                  builder: (context, snapshot) { //AsyncSnapshot
+                    if(snapshot.hasData){ //snapshot.hasData returns true if contains a non-null data value.
                         return
                           Column (
                               crossAxisAlignment: CrossAxisAlignment.start,
